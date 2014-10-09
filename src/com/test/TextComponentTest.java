8b8a572c8a815fa52e41fedd34f1303e94428c17
Gerald Caldwell <Gerald.Caldwell@MAK-Solutions.com>
2014-10-09 16:13:05 -0400

    added text fields and labels.

--- src/com/test/TextComponentTest.java
+++ src/com/test/TextComponentTest.java
@@ -3,8 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.test;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 /**
  *
@@ -16,8 +19,63 @@ public class TextComponentTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        EventQueue.invokeLater(new Runnable() {
+
+            public void run() {
+                TextComponentFrame frame = new TextComponentFrame();
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+            }
+        });
     }
-    
-    
+}
+
+/**
+ * A frame with sample text components
+ */
+class TextComponentFrame extends JFrame {
+
+    public static final int DEFAULT_WIDTH = 300;
+    public static final int DEFAULT_HEIGHT = 300;
+
+    public TextComponentFrame() {
+
+        setTitle("Text ComponentTest");
+        setSize(DEFAULT_HEIGHT, DEFAULT_HEIGHT);
+
+        final JTextField textField = new JTextField();
+        final JPasswordField passwordField = new JPasswordField();
+
+        JPanel northPanel = new JPanel();
+        northPanel.setLayout(new GridLayout(2, 2));
+        northPanel.add(new JLabel("User name: ", SwingConstants.RIGHT));
+        northPanel.add(textField);
+        northPanel.add(new JLabel("Pasword: ", SwingConstants.RIGHT));
+        northPanel.add(passwordField);
+
+        add(northPanel, BorderLayout.NORTH);
+
+        final JTextArea textArea = new JTextArea(8, 40);
+        JScrollPane scrollPane = new JScrollPane(textArea);
+
+        add(scrollPane, BorderLayout.CENTER);
+
+        // Add button to append text into the text area
+        JPanel southPanel = new JPanel();
+
+        JButton insertButton = new JButton("Insert");
+        southPanel.add(insertButton);
+
+        insertButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                textArea.append("User name: " + textField.getText() + " Password: "
+                        + new String(passwordField.getPassword()) + "\n");
+
+            }
+        });
+
+        add(southPanel, BorderLayout.SOUTH);
+
+    }
+
 }
